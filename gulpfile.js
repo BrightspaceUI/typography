055b0095587cb6cd69f27eb7cd542953c0532694
Dave Batiste <dave.batiste@d2l.com>
2014-09-19 18:09:47 -0400

    Refactoring typography styles into mixins.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require( 'gulp' ),
+	vui = require( 'vui-helpers' );
+
+gulp.task( 'clean', function() {
+	return vui.clean( [ 'dist/**/*', 'lib', 'output' ] );
+} );
+
+gulp.task( 'css', function () {
+	return vui.makeCss( 'src/**/*.style', 'dist/', { lintOpts: '.csslintrc' } );
+} );
+
+gulp.task( 'less', function () {
+	return vui.makeLess( 'src/**/*.less', 'dist/' );
+} );
+
+gulp.task( 'default', [ 'clean' ], function() {
+	gulp.start( 'css', 'less' );
+} );
+
+gulp.task( 'test', function () {
+	return vui.test(
+			'test/unit/karma.conf.js',
+			'test/unit/**/*Spec.js',
+			'dist/**/*.css'
+		);
+} );
