03ae7feee4f0f10ad7dc9a8b8f073c8dcd1e42fc
Dave Batiste <dave.batiste@d2l.com>
2014-09-29 11:15:56 -0400

    Moving src to root.

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,22 @@ var gulp = require( 'gulp' ),
 	vui = require( 'vui-helpers' );
 
 gulp.task( 'clean', function() {
-	return gulp.src( [ 'dist', 'output' ], { read: false } )
+	return gulp.src( [ 'typography.css' ], { read: false } )
 		.pipe( vui.clean() );
 } );
 
 gulp.task( 'css', function () {
-	return vui.makeCss( 'src/**/*.style', 'dist/', { lintOpts: '.csslintrc' } );
-} );
-
-gulp.task( 'less', function () {
-	return vui.makeLess( 'src/**/*.less', 'dist/' );
+	return vui.makeCss( 'typography.css.less', 'typography.css', { lintOpts: '.csslintrc' } );
 } );
 
 gulp.task( 'default', [ 'clean' ], function() {
-	gulp.start( 'css', 'less' );
+	gulp.start( 'css' );
 } );
 
 gulp.task( 'test', function () {
 	return vui.test(
 			'test/unit/karma.conf.js',
 			'test/unit/**/*Spec.js',
-			'dist/**/*.css'
+			'typography.css'
 		);
 } );
