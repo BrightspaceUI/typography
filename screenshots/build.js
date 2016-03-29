80cee6cf411c8ee0e5f0f8e5cf71476b4b676c65
Dave Batiste <dave.batiste@d2l.com>
2016-03-29 10:04:27 -0400

    Add web components for typography.

--- screenshots/build.js
+++ screenshots/build.js
@@ -4,8 +4,11 @@ page.viewportSize = {width: 750, height: 1000};
 function capture(name, width) {
 
 	var clipRect = page.evaluate(function(n) {
-		return document.querySelector('.screenshot-' + n)
-			.getBoundingClientRect();
+		var thing = document.querySelector('.screenshot-' + n);
+		thing.style.display = 'inline-block';
+		return thing.getBoundingClientRect();
+		//return document.querySelector('.screenshot-' + n)
+		//	.getBoundingClientRect();
 	}, name);
 
 	page.clipRect = {
@@ -14,13 +17,14 @@ function capture(name, width) {
 		width:  width || clipRect.width,
 		height: clipRect.height
 	};
+
 	page.render('./screenshots/' + name + '.png');
 
 }
 
-page.open('./test/typography.html', function() {
+page.open('./test/perceptual/typography.html', function() {
 	capture('paragraph');
-	capture('headings', 200);
+	capture('headings', 1200);
 	capture('small', 200);
 	capture('small-strong', 200);
 	phantom.exit();
