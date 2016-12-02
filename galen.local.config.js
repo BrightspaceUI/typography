9edb8f554b61d5784e250347dc175e9246dddd5a
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2016-12-02 10:36:45 -0500

    Update d2l-galen-util

--- galen.local.config.js
+++
@@ -1,9 +0,0 @@
-/* global LocalBrowserFactory, load */
-
-this.browsers = {
-	phantomjs: new LocalBrowserFactory({
-		browser: 'phantomjs'
-	})
-};
-
-load('galen.common.config.js');
--- typography.test.js
+++ typography.test.js
@@ -1,6 +1,10 @@
-/* global SauceBrowserFactory, load */
+/* eslint no-invalid-this: 0 */
+'use strict';
 
-this.browsers = {
+var browsers = {
+	phantomjs: new LocalBrowserFactory({
+		browser: 'phantomjs'
+	}),
 	chromeWindows: new SauceBrowserFactory({
 		browser: 'Chrome',
 		platform: 'WIN10'
@@ -37,4 +41,21 @@ this.browsers = {
 	})
 };
 
-load('galen.common.config.js');
+var endpoint = 'http://localhost:8080/components/d2l-typography/test/perceptual/typography.html';
+var spec = 'test/font-classes.gspec';
+
+polymerTests(browsers, function(test) {
+	test('typography-mobile', {
+		endpoint: endpoint,
+		spec: spec,
+		size: '320x600',
+		tags: ['mobile']
+	});
+
+	test('typography-desktop', {
+		endpoint: endpoint,
+		spec: spec,
+		size: '1024x768',
+		tags: ['desktop']
+	});
+});
